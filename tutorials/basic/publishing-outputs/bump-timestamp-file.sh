d547c0931efe45e6f2ec582771f878874fa98671
Brian Demant <brian.demant@gmail.com>
2018-04-10 15:50:23 +0200

    changes

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -7,6 +7,7 @@ git clone resource-gist updated-gist
 
 cd updated-gist
 echo $(date) > bumpme
+echo "brian was here" >> bumpme
 
 git config --global user.email "nobody@concourse-ci.org"
 git config --global user.name "Concourse"
